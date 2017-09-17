1f9288fb992ab2e5537ded155204689be1308071
ethanelstein <ethanelstein1@gmail.com>
2017-09-17 21:53:42 +0100

    seit

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,10 @@
-var myHeading = document.querySelector('h1');
-myHeading.textContent = 'Hello world!';
+var myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    var mySrc = myImage.getAttribute('src');
+    if(mySrc === 'images/bald_eagle.jpg') {
+      myImage.setAttribute ('src','images/gorilla.jpg');
+    } else {
+      myImage.setAttribute ('src','images/firefox-icon.png');
+    }
+}
